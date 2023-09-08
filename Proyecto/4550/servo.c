6b3e5fb736de7fb865875d998233c71e24731306
vhperaltaj <victor.peralta@microside.com>
2023-09-08 10:33:17 -0600

    Fixed indentation

--- Proyecto/4550/servo.c
+++ Proyecto/4550/servo.c
@@ -19,38 +19,34 @@ Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 
 void main() org 0x2000
 {
-     LATC2_bit = 0;         //Se asigna el pin como salida
-     TRISC = 0;             //Se establece el pin en un estado bajo
-     
-  do
-  {
-    unsigned int i;
-    for(i=0;i<50;i++)       //Posiciona el servomotor en 0°
-    {
-      pin_servo = 1;
-      Delay_us(500);
-      pin_servo = 0;
-      Delay_us(19500);
-    }
-    
-    Delay_ms(2000);
+   LATC2_bit = 0;                  // Se asigna el pin como salida
+   TRISC = 0;                      // Se establece el pin en un estado bajo
 
-    for(i=0;i<50;i++)       //Posiciona el servomotor en 90°
-    {
-      pin_servo = 1;
-      Delay_us(1500);
-      pin_servo = 0;
-      Delay_us(18500);
-    }
-    
-    Delay_ms(2000);
-      
-    for(i=0;i<50;i++)       //Posiciona el servomotor en 180°
-    {
-      pin_servo = 1;
-      Delay_us(2500);
-      pin_servo = 0;
-      Delay_us(17500);
-    }
-  }while(1);
+   do {
+      unsigned int i;
+      for ( i = 0; i < 50; i++ ) { // Posiciona el servomotor en 0°
+         pin_servo = 1;
+         Delay_us( 500 );
+         pin_servo = 0;
+         Delay_us( 19500 );
+      }
+
+      Delay_ms( 2000 );
+
+      for ( i = 0; i < 50; i++ ) { // Posiciona el servomotor en 90°
+         pin_servo = 1;
+         Delay_us( 1500 );
+         pin_servo = 0;
+         Delay_us( 18500 );
+      }
+
+      Delay_ms( 2000 );
+
+      for ( i = 0; i < 50; i++ ) { // Posiciona el servomotor en 180°
+         pin_servo = 1;
+         Delay_us( 2500 );
+         pin_servo = 0;
+         Delay_us( 17500 );
+      }
+   } while ( 1 );
 }
